0bc8c66de9e56ef66d0b133b582deb225a1bfadd
doronh8 <doron.h8j@gmail.com>
2025-07-25 19:52:19 +0300

    changing source to ness folder

--- scheduler/execute_my_kaggle_daily.sh
+++ scheduler/execute_my_kaggle_daily.sh
@@ -4,9 +4,9 @@
 set -e
 
 # Run the python script according to our configuration
-cd ~/workspace/bi/
+cd ~/workspace/bi/jobs/ness/
 
 
 # bash ~/workspace/bi/jobs/ness/scheduler/execute_my_kaggle_daily.sh
 
-python3 jobs/ness/my_kaggle.py bi-course-461012 --etl-name etl --etl-action daily
+python3 my_kaggle.py bi-course-461012 --etl-name etl --etl-action daily
